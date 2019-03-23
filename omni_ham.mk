aa1602b819307bd44afc59b11223ca121cc6baa3
dianlujitao <dianlujitao@lineageos.org>
2019-03-23 16:17:36 +0800

    ham: Fix HW FDE when building with Pie tree
     * ro.board.platform is no longer part of recovery ramdisk on Pie, thus
       hw_get_module_by_class would fail to find the keystore module and HW
       FDE is broken.
     * To address the issue, use a property to specify the HAL class to make
       the HAL picked by libhardware.
    
    Change-Id: I2b09a06be34ca66a400e98fce8714226ae5f3d10

--- omni_ham.mk
+++ omni_ham.mk
@@ -26,6 +26,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8974
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ham
 PRODUCT_NAME := omni_ham
